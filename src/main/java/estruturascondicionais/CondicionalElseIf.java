ecfa30717cb122c1dc460f068522caf6c5a185eb
Bruno Bilheri Monteiro <brunobilheri@hotmail.com>
2023-11-25 16:18:23 -0300

    Condicionais

---
+++ src/main/java/estruturascondicionais/CondicionalElseIf.java
@@ -0,0 +1,15 @@
+package estruturascondicionais;
+
+public class CondicionalElseIf {
+    // Condicional Else If
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("Bom dia");
+
+        if (x < 0) {
+            System.out.println("Boa tarde");
+        } else {
+            System.out.println("Boa noite");
+        }
+    }
+}
---
+++ src/main/java/estruturascondicionais/CondicionalIf.java
@@ -0,0 +1,15 @@
+package estruturascondicionais;
+
+public class CondicionalIf {
+    // Condicional If
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("Bom dia");
+
+        if (x < 0) {
+            System.out.println("Boa tarde");
+        }
+
+        System.out.println("Boa noite");
+    }
+}
